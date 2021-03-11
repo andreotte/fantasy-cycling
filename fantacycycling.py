569bbd9d1d252c9b0c50aa47d1b997d13bd0ef36
Andre Otte <42424851+andreotte@users.noreply.github.com>
2021-03-11 07:15:33 -0500

    just get the scores for the riders in our league

--- fantacycycling.py
+++ fantacycycling.py
@@ -6,11 +6,12 @@ import os
 from bs4 import BeautifulSoup
 
 def getRidersScores():
-    ridersScores = []
+    # ridersScores = []
+    ridersScores = {}
     offset = 0
     results = True
     while(results):
-        URL = f'https://www.procyclingstats.com/rankings.php?date=2021-03-09&nation=&age=&zage=&page=smallerorequal&team=&offset={offset}&filter=Filter&p=me&s=season-individual'
+        URL = f'https://www.procyclingstats.com/rankings.php?date=2021-03-11&nation=&age=&zage=&page=smallerorequal&team=&offset={offset}&filter=Filter&p=me&s=season-individual'
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -22,11 +23,22 @@ def getRidersScores():
             for i in range(len(riderString)):
                 riderName = riderString[i]['href'][6:].replace('-', ' ')
                 riderScore = pointsString[i].string
-                ridersScores.append([riderName, riderScore])
+                # ridersScores.append([riderName, riderScore])
+                ridersScores[riderName] = riderScore
         else:
             results = False
         
     return ridersScores 
+ 
+def buildLeagueScores(leagueRiders, riderScores):
+    leagueScores = []
+    for lr in leagueRiders:
+        try:
+            leagueScores.append([lr, riderScores[lr.lower()]])
+        except KeyError:
+            leagueScores.append([lr,0])
+
+    return leagueScores
 
 def writeRiderScores(rows):
     scriptDir = os.path.dirname(__file__) #<-- absolute dir the script is in
@@ -35,18 +47,112 @@ def writeRiderScores(rows):
         writer = csv.writer(csvfile)  
 
         for row in rows:
-            print(row)
             writer.writerow(row)
 
-def main():
-    writeRiderScores(getRidersScores())
+def getRidersFromLeague(): #todo read this from a file?
+    riders = ["giacomo nizzolo"
+    ,"mathieu van der poel"
+    ,"mads pedersen"
+    ,"wout van aert"
+    ,"sam bennett"
+    ,"arnaud demare"
+    ,"marc hirschi"
+    ,"dan martin"
+    ,"richard carapaz"
+    ,"julian alaphilippe"
+    ,"primoz roglic"
+    ,"mikel landa"
+    ,"filippo ganna"
+    ,"tadej pogacar"
+    ,"alexander kristoff"
+    ,"bryan coquard"
+    ,"peter sagan"
+    ,"stefan kung"
+    ,"jakob fuglsang"
+    ,"wilco kelderman"
+    ,"tao geoghegan hart"
+    ,"egan bernal"
+    ,"giulio ciccone"
+    ,"guillaume martin"
+    ,"alejandro valverde"
+    ,"remco evenepoel"
+    ,"daryl impey"
+    ,"tim wellens"
+    ,"enric mas"
+    ,"aleksey lutsenko"
+    ,"hugh carthy"
+    ,"michael woods"
+    ,"thomas pidcock"
+    ,"chris froome"
+    ,"peerapol chawchiangkwang"
+    ,"remi cavagna"
+    ,"davide ballerini"
+    ,"geraint thomas"
+    ,"oliver naesen"
+    ,"elia viviani"
+    ,"jai hindley"
+    ,"kasper asgreen"
+    ,"neilson powless"
+    ,"thomas de gendt"
+    ,"sepp kuss"
+    ,"niccolo bonifazio"
+    ,"josef cerny"
+    ,"richie porte"
+    ,"biniam ghirmay"
+    ,"michael matthews"
+    ,"sonny colbrelli"
+    ,"simon yates"
+    ,"rohan dennis"
+    ,"caleb ewan"
+    ,"davide formolo"
+    ,"diego ulissi"
+    ,"nairo quintana"
+    ,"jasper stuyven"
+    ,"tiesj benoot"
+    ,"ben o connor"
+    ,"sergio higuita"
+    ,"benoit cosnefroy"
+    ,"romain bardet"
+    ,"maximilian schachmann"
+    ,"jack haig"
+    ,"greg van avermaet"
+    ,"matteo trentin"
+    ,"tejay van garderen"
+    ,"quinn simmons"
+    ,"aleksandr vlasov"
+    ,"david gaudu"
+    ,"warren barguil"
+    ,"philippe gilbert"
+    ,"george bennett"
+    ,"magnus cort"
+    ,"miguel angel lopez"
+    ,"michal kwiatkowski"
+    ,"thibaut pinot"
+    ,"alberto bettiol"
+    ,"pascal ackermann"
+    ,"fernando gaviria"
+    ,"adam yates"
+    ,"tim merlier"
+    ,"vincenzo nibali"
+    ,"rafal majka"
+    ,"matej mohoric"
+    ,"hugo hofstetter"
+    ,"florian senechal"
+    ,"joao almeida"
+    ,"soren kragh andersen"
+    ,"steven kruijswijk"
+    ,"lennard kamna"
+    ,"yves lampaert"
+    ,"patrick konrad"
+    ,"pavel sivakov"
+    ,"luka mezgec"]
+
+    return riders
+
+def main(): 
+    leagueScores = buildLeagueScores(getRidersFromLeague(),getRidersScores())
+    writeRiderScores(leagueScores)
     
 if __name__ == '__main__':
     main()
 
-
-
-
-
-
-# pp.pprint(soup)
